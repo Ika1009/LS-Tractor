85f92d7247d4552835802e75bb404005d5d8782b
milosmitrovic20 <milosmitrovic005@gmail.com>
2025-03-06 23:40:14 +0100

    fix: improve dropdown functionality and ensure proper event handling

--- traktor.js
+++ traktor.js
@@ -211,14 +211,16 @@ document.addEventListener('DOMContentLoaded', function() {
         pswpModule: PhotoSwipe 
       });
       lightbox.init();
+});
 
+document.addEventListener("DOMContentLoaded", function () {
     const dropdownButton = document.getElementById("mega-menu-full-dropdown-button");
     const dropdownMenu = document.getElementById("mega-menu-full-dropdown");
 
     dropdownButton.addEventListener("click", function () {
         dropdownMenu.classList.toggle("hidden");
     });
-
+    
     document.addEventListener("click", function (event) {
         if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
             dropdownMenu.classList.add("hidden");
