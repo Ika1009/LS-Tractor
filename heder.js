21b6739c76c6154eb296804b10a63c3da8b6456f
milosmitrovic20 <milosmitrovic005@gmail.com>
2025-03-28 02:09:29 +0100

    dropdown menu animation

--- heder.js
+++ heder.js
@@ -3,20 +3,28 @@ document.addEventListener("DOMContentLoaded", function () {
     const dropdownMenu = document.getElementById("mega-menu-full-dropdown");
     const menuToggle = document.querySelector('[data-collapse-toggle="mobile-menu-2"]');
     const menu = document.querySelector("#mega-menu-full");
+    let timeout;
 
     dropdownButton.addEventListener("mouseenter", function () {
         if (window.innerWidth >= 1024) { 
+            clearTimeout(timeout); 
             dropdownMenu.classList.remove("hidden");
-            dropdownMenu.classList.add("block");
-        } else {
-            window.location.href = "/masine.html";
+
+            setTimeout(() => { 
+                dropdownMenu.classList.remove("opacity-0", "translate-y-[-10px]");
+                dropdownMenu.classList.add("opacity-100", "translate-y-0");
+            }, 100); 
         }
     });
 
     dropdownMenu.addEventListener("mouseleave", function () {
         if (window.innerWidth >= 1024) {
-            dropdownMenu.classList.remove("block");
-            dropdownMenu.classList.add("hidden");
+            dropdownMenu.classList.remove("opacity-100", "translate-y-0");
+            dropdownMenu.classList.add("opacity-0", "translate-y-[-10px]");
+
+            timeout = setTimeout(() => {
+                dropdownMenu.classList.add("hidden");
+            }, 300);
         }
     });
 
